3b0216f63e627f5e6faf315814ec499631693f57
carloshenriqds18 <carlos.henriqds18@hotmail.com>
2019-08-14 03:35:33 +0900

    função masculino

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,19 @@
-// This is a JavaScript file
+$(document).on("click","#resultado",function(){
+
+var peso;
+var altura;
+var nivel;
+var resultado;
+var calcular;
+
+idade = $("#idadeH").val(); 
+peso = $("#pesoH").val(); 
+altura = $("#alturaH").val(); 
+atividade = $("#atividade").val(); 
+resultado = $("#resultado").val();
+
+$resultado = atividade * (66 + ((13,7 * peso) + (5 * altura) - ( 6.8 * idade)));
+$("#visor").val (resultado);
+
+});
 
